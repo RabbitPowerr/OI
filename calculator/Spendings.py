ad5786fac44008b205fa2b4d7999c239c7b97cf9
RabbitPowerr <rabbitpowerr@ya.ru>
2021-10-16 21:23:08 +0300

    Fix wrong calc

--- calculator/Spendings.py
+++ calculator/Spendings.py
@@ -6,19 +6,19 @@ class Spendings():
     @staticmethod
     def Calc(data):
         nmonth=12
-        years= data['Years']
         total={'Online':0,'Offline':0}
         for name,x in data['Spending'].items():
             p=1.0
-            n=4*nmonth
-            #print(x)
+            n=data['Years']*nmonth
+           # print(total,x)
+            
             for i in range(n):
                 if(x['Need_in_online']== True ):
-                    total['Online']+=(x['Cost']/x['Regularyty'])*p
+                    total['Online']+=(x['Cost']/x['Regularyty'])*p *(1/x['Num_groups'])
                 if(x['Need_in_offline']== True):
-                    total['Offline']=(x['Cost']/x['Regularyty'])*p
-                if(i%12==0):
-                    p*=(1+data['Inflation'])
+                    total['Offline']+=(x['Cost']/x['Regularyty'])*p*(1/x['Num_groups'])
+               # if(i%12==0):
+                #    p*=(1+data['Inflation'])
         return total
 
     @staticmethod
@@ -27,7 +27,7 @@ class Spendings():
         month=6
         p=1.0
         for x in data['num_of_subjects']:
-            n = x/2
+            n = (x)/data['Avaliable_thours'] 
             total+=p*n*data['Salary']*month  
             p*=data['Inflation']
         return total
--- calculator/calculator.py
+++ calculator/calculator.py
@@ -8,8 +8,10 @@ class Calculator():
     def Get_price(data):
         total=Spendings().Calc(data)
         cost_t= Spendings.Salary(data)
+        #print(total)
         total['Online']+=cost_t
         total['Offline']+=cost_t
+        #print(total)
         total['Online']/=data['Num_students']
         total['Offline']/=data['Num_students']
         return total
